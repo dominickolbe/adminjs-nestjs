047a97f533221a245ddc6f0f9eea25d494344559
Dominic Kolbe <kolbedominic@gmail.com>
2023-11-10 21:21:10 +0000

    cleanup project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { ConfigModule } from "@nestjs/config";
+import { validate } from "./config/env.validation";
+import configuration from "./config/configuration";
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ validate, isGlobal: true, load: [configuration] })],
   controllers: [AppController],
   providers: [AppService],
 })
